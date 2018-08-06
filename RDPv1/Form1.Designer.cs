c36d42120a0e29c4c0c820739f6e01287f50550b
Depthperception2166 <jwm2166@gmail.com>
2018-08-06 08:49:55 -0500

    Adding RDP clientV1

---
+++ RDPv1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace RDPv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rdp = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.psswordBox = new System.Windows.Forms.TextBox();
+            this.connectbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.disconnectbtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdp
+            // 
+            this.rdp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdp.Enabled = true;
+            this.rdp.Location = new System.Drawing.Point(3, 27);
+            this.rdp.Name = "rdp";
+            this.rdp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdp.OcxState")));
+            this.rdp.Size = new System.Drawing.Size(1573, 587);
+            this.rdp.TabIndex = 0;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.usernameBox.Location = new System.Drawing.Point(302, 636);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameBox.TabIndex = 2;
+            // 
+            // psswordBox
+            // 
+            this.psswordBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.psswordBox.Location = new System.Drawing.Point(502, 636);
+            this.psswordBox.Name = "psswordBox";
+            this.psswordBox.PasswordChar = '*';
+            this.psswordBox.Size = new System.Drawing.Size(100, 20);
+            this.psswordBox.TabIndex = 3;
+            // 
+            // connectbtn
+            // 
+            this.connectbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.connectbtn.Location = new System.Drawing.Point(650, 636);
+            this.connectbtn.Name = "connectbtn";
+            this.connectbtn.Size = new System.Drawing.Size(75, 23);
+            this.connectbtn.TabIndex = 4;
+            this.connectbtn.Text = "Connect";
+            this.connectbtn.UseVisualStyleBackColor = true;
+            this.connectbtn.Click += new System.EventHandler(this.connectbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 641);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Server Name";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 641);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(427, 642);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1578, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // disconnectbtn
+            // 
+            this.disconnectbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.disconnectbtn.Location = new System.Drawing.Point(762, 636);
+            this.disconnectbtn.Name = "disconnectbtn";
+            this.disconnectbtn.Size = new System.Drawing.Size(75, 23);
+            this.disconnectbtn.TabIndex = 12;
+            this.disconnectbtn.Text = "Disconnect";
+            this.disconnectbtn.UseVisualStyleBackColor = true;
+            this.disconnectbtn.Click += new System.EventHandler(this.disconnectbtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "192.168.254.5"});
+            this.comboBox1.Location = new System.Drawing.Point(82, 635);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(1578, 673);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.disconnectbtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.connectbtn);
+            this.Controls.Add(this.psswordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.rdp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting rdp;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox psswordBox;
+        private System.Windows.Forms.Button connectbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button disconnectbtn;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ RDPv1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MSTSCLib;
+using AxMSTSCLib;
+
+namespace RDPv1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connectbtn_Click(object sender, EventArgs e)
+        {
+
+            rdp.Server = comboBox1.SelectedItem.ToString(); 
+            rdp.UserName = usernameBox.Text;
+            IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+            secured.ClearTextPassword = psswordBox.Text;
+            rdp.Connect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+
+        private void disconnectbtn_Click(object sender, EventArgs e) //Disconnects RDP connection
+        {
+            if (rdp.Connected.ToString() == "1")
+            {
+                rdp.Disconnect();
+            }
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem.ToString();
+        }
+
+       
+
+    }
+}
